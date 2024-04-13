799d028971ebb51b85463da3169f3587606355e7
Skeeser <1326658417@qq.com>
2024-04-13 16:57:50 +0800

    finished rpc manager server

--- rpc_manager/server/rpc_manager.cpp
+++ rpc_manager/server/rpc_manager.cpp
@@ -1,30 +1,34 @@
 #include "rpc_manager.h"
 
-namespace monitor{
-GrpcManagerImpl::GrpcManagerImpl() {}
+namespace monitor
+{
+    GrpcManagerImpl::GrpcManagerImpl()
+    {
+        // 初始化日志
+        Log::init(1, "../log/server", ".log", 1024);
+    }
 
-GrpcManagerImpl::~GrpcManagerImpl() {}
+    GrpcManagerImpl::~GrpcManagerImpl() {}
 
-::grpc::Status SetMonitorInfo(::grpc::ServerContext* context,
-        const ::monitor::proto::MonitorInfo* request,
-        ::google::protobuf::Empty* response){
-    monitor_infos_.Clear();
-    // 赋值
-    monitor_infos_ = *request;
-    // todo: 通过日志写入
-    std::cout << "into" << request->soft_irq_size() << std::endl;
-    
-    return ::grpc::Status::OK;
+    ::grpc::Status SetMonitorInfo(::grpc::ServerContext *context,
+                                  const ::monitor::proto::MonitorInfo *request,
+                                  ::google::protobuf::Empty *response)
+    {
+        monitor_infos_.Clear();
+        // 赋值
+        monitor_infos_ = *request;
+        // 通过日志写入
+        // std::cout << "into" << request->soft_irq_size() << std::endl;
+        LOG_INFO("Soft_irq_size: %d", request->soft_irq_size());
+        return ::grpc::Status::OK;
+    }
+
+    ::grpc::Status GetMonitorInfo(::grpc::ServerContext *context,
+                                  const ::google::protobuf::Empty *request,
+                                  ::monitor::proto::MonitorInfo *response)
+    {
+        *response = monitor_infos_;
+        return ::grpc::Status::OK;
+    }
 
 }
-
-
-::grpc::Status GetMonitorInfo(::grpc::ServerContext* context,
-                                const ::google::protobuf::Empty* request,
-                                ::monitor::proto::MonitorInfo* response);
-
-
-
-}
-
-
--- rpc_manager/server/rpc_manager.h
+++ rpc_manager/server/rpc_manager.h
@@ -5,26 +5,27 @@
 
 #include "monitor_info.grpc.pb.h"
 #include "monitor_info.pb.h"
+#include "log_manager.h"
 
 // 写rpc manager的服务代码
-namespace monitor{
-class GrpcManagerImpl : public monitor::proto::GrpcManager::Service{
+namespace monitor
+{
+    class GrpcManagerImpl : public monitor::proto::GrpcManager::Service
+    {
     public:
-    GrpcManagerImpl();
-    virtual ~GrpcManagerImpl();
+        GrpcManagerImpl();
+        virtual ~GrpcManagerImpl();
 
-    // 返回全局命名空间的grpc的状态
-    ::grpc::Status SetMonitorInfo(::grpc::ServerContext* context,
-                                const ::monitor::proto::MonitorInfo* request,
-                                ::google::protobuf::Empty* response);
-    ::grpc::Status GetMonitorInfo(::grpc::ServerContext* context,
-                                const ::google::protobuf::Empty* request,
-                                ::monitor::proto::MonitorInfo* response);
+        // 返回全局命名空间的grpc的状态
+        ::grpc::Status SetMonitorInfo(::grpc::ServerContext *context,
+                                      const ::monitor::proto::MonitorInfo *request,
+                                      ::google::protobuf::Empty *response);
+        ::grpc::Status GetMonitorInfo(::grpc::ServerContext *context,
+                                      const ::google::protobuf::Empty *request,
+                                      ::monitor::proto::MonitorInfo *response);
 
     private:
-    monitor::proto::MonitorInfo monitor_infos_;
-
-}
-
+        monitor::proto::MonitorInfo monitor_infos_;
+    }
 
 }
