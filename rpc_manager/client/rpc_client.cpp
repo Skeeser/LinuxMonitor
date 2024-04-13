7f595a1636f08a2f78f7af46714b358ec39d3eff
Skeeser <1326658417@qq.com>
2024-04-13 20:21:12 +0800

    finished rpc client

---
+++ rpc_manager/client/rpc_client.cpp
@@ -0,0 +1,67 @@
+#include "rpc_client.h"
+
+namespace monitor
+{
+    RpcClient::RpcClient()
+    {
+        // 初始化日志
+        Log::Instance()->init(1, "../log/client", ".log", 1024);
+        FILE *fp;
+        fp = fopen("ip_address.txt", "r");
+        char ipStr[50];
+        bool isOpen = false;
+        if (fp != nullptr && fgets(ipStr, 50, fp) != nullptr)
+        {
+            isOpen = true;
+            fclose(fp);
+            int pos = strlen(ipStr);
+            ipStr[pos - 1] = '\0'; // 去除换行符
+        }
+        else
+        {
+            strcpy(ipStr, "localhost:50051");
+        }
+
+        auto channel = ::grpc::CreateChannel(ipStr, ::grpc::InsecureChannelCredentials());
+        stubPtr_ = monitor::proto::GrpcManager::NewStub(channel);
+    }
+
+    RpcClient::~RpcClient()
+    {
+    }
+
+    void RpcClient::SetMonitorInfo(const monitor::proto::MonitorInfo &monito_info)
+    {
+        ::grpc::ClientContext context;
+        ::google::protobuf::Empty response;
+        ::grpc::Status status =
+            stubPtr_->SetMonitorInfo(&context, monito_info, &response);
+        if (status.ok())
+        {
+        }
+        else
+        {
+            // std::cout << status.error_details() << std::endl;
+            // std::cout << "falied to connect !!!" << std::endl;
+            LOG_ERROR("Detail: %s", status.error_details().c_str());
+            LOG_ERROR("Falied to connect !!!");
+        }
+    }
+    void RpcClient::GetMonitorInfo(monitor::proto::MonitorInfo *monito_info)
+    {
+        ::grpc::ClientContext context;
+        ::google::protobuf::Empty request;
+        ::grpc::Status status =
+            stubPtr_->GetMonitorInfo(&context, request, monito_info);
+        if (status.ok())
+        {
+        }
+        else
+        {
+            // std::cout << status.error_details() << std::endl;
+            // std::cout << "falied to connect !!!" << std::endl;
+            LOG_ERROR("Detail: %s", status.error_details().c_str());
+            LOG_ERROR("Falied to connect !!!");
+        }
+    }
+}
